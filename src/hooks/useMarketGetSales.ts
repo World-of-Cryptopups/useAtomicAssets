fe80d418fa2f0aeb3e073217998f62c96e2cc6a7
TheBoringDude <iamcoderx@gmail.com>
2022-01-22 14:11:15 +0800

    Add hook `useMarketGetSales`.

---
+++ src/hooks/useMarketGetSales.ts
@@ -0,0 +1,79 @@
+import { buildDataOptions } from '../lib/data'
+import {
+  DataOptions,
+  DateBoundaryParams,
+  GreylistParams,
+  PrimaryBoundaryParams
+} from '../typings/atomicassets-js'
+import { ISales } from '../typings/custom'
+import { FetchResult } from '../typings/fetch'
+import { PageLimitOrderParams } from '../typings/params'
+import useAtomicGetter from './useAtomicGetter'
+
+interface useMarketGetSalesProps
+  extends GreylistParams,
+    PrimaryBoundaryParams,
+    DateBoundaryParams,
+    PageLimitOrderParams {
+  state?: string
+  max_assets?: number
+  min_assets?: number
+  show_seller_contracts?: boolean
+  contract_whitelist?: string
+  seller_blacklist?: string
+  buyer_blacklist?: string
+  asset_id?: number
+  marketplace?: string
+  maker_marketplace?: string
+  taker_marketplace?: string
+  symbol?: string
+  account?: string
+  seller?: string
+  buyer?: string
+  min_price?: number
+  max_price?: number
+  min_template_mint?: number
+  max_template_mint?: number
+  collection_name?: string
+  schema_name?: string
+  template_id?: number
+  is_transferable?: boolean
+  is_burnable?: boolean
+  burned?: boolean
+  owner?: string
+  match?: string
+  match_immutable_name?: string
+  match_mutable_name?: string
+  sort?: 'created' | 'updated' | 'sale_id' | 'price' | 'template_mint'
+}
+
+/**
+ * Get market sales.
+ *
+ * @param props Query options.
+ * @param dataOptions Custom query options for asset / template data fields.
+ * @param endpoint Atomicassets endpoint.
+ * @returns FetchResult<ISales[]>
+ */
+const useMarketGetSales = (
+  props?: useMarketGetSalesProps | null,
+  dataOptions?: DataOptions,
+  endpoint?: string
+): FetchResult<ISales[]> => {
+  let dOptions: Record<string, any> = {}
+  if (dataOptions != null) {
+    dOptions = buildDataOptions(dataOptions)
+  }
+
+  return useAtomicGetter<ISales[]>(
+    props != null
+      ? {
+          uri: '/atomicmarket/v1/sales',
+          params: { ...props, ...dOptions },
+          endpoint
+        }
+      : null
+  )
+}
+
+export default useMarketGetSales
--- src/typings/custom.ts
+++ src/typings/custom.ts
@@ -1,6 +1,92 @@
 // custom typings
 
+import { ILightSchema } from './atomicassets-js'
+
 export interface IAccountsProps {
   account: string
   assets: string
+}
+
+export interface ISales {
+  market_contract: string
+  assets_contract: string
+  sale_id: string
+  seller: string
+  buyer: string
+  offer_id: string
+  price: Price
+  listing_price: number
+  listing_symbol: string
+  assets: Asset[]
+  maker_marketplace: string
+  taker_marketplace: string
+  collection: Collection
+  state: number
+  updated_at_block: string
+  updated_at_time: string
+  created_at_block: string
+  created_at_time: string
+}
+
+export interface Asset {
+  contract: string
+  asset_id: string
+  owner: string
+  name: string
+  is_transferable: boolean
+  is_burnable: boolean
+  template_mint: string
+  collection: Collection
+  schema: ILightSchema
+  template: Template
+  backed_tokens: Price[]
+  immutable_data: Data
+  mutable_data: Data
+  data: Data
+  burned_by_account: string
+  burned_at_block: string
+  burned_at_time: string
+  updated_at_block: string
+  updated_at_time: string
+  transferred_at_block: string
+  transferred_at_time: string
+  minted_at_block: string
+  minted_at_time: string
+}
+
+export interface Price {
+  token_contract: string
+  token_symbol: string
+  token_precision: number
+  amount: string
+}
+
+export interface Collection {
+  collection_name: string
+  name: string
+  author: string
+  allow_notify: boolean
+  authorized_accounts: string[]
+  notify_accounts: string[]
+  market_fee: number
+  created_at_block: string
+  created_at_time: string
+}
+
+export type Data = Record<string, any>
+
+export interface Format {
+  name: string
+  type: string
+}
+
+export interface Template {
+  template_id: string
+  max_supply: string
+  issued_supply: string
+  is_transferable: boolean
+  is_burnable: boolean
+  immutable_data: Data
+  created_at_time: string
+  created_at_block: string
 }

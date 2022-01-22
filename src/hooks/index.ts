7b793b5fb89b026172199837b3ccec5e83c44d25
TheBoringDude <iamcoderx@gmail.com>
2022-01-22 14:22:50 +0800

    Export hook functions to `index.ts`

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,21 @@
 import useAtomicGetter from './useAtomicGetter'
+import useGetAccount from './useGetAccount'
+import useGetAccounts from './useGetAccounts'
+import useGetAssetID from './useGetAssetID'
 import useGetAssets from './useGetAssets'
+import useGetCollections from './useGetCollections'
+import useGetSchemas from './useGetSchemas'
+import useGetTemplates from './useGetTemplates'
+import useMarketGetSales from './useMarketGetSales'
 
-export { useAtomicGetter, useGetAssets }
+export {
+  useAtomicGetter,
+  useGetAssets,
+  useGetAccount,
+  useGetAccounts,
+  useGetAssetID,
+  useGetCollections,
+  useGetSchemas,
+  useGetTemplates,
+  useMarketGetSales
+}

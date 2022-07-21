ca0e1bf1d1a0c34c6111376f409649c6d6588bca
TheBoringDude <iamcoderx@gmail.com>
2022-07-21 14:12:09 +0800

    fix: export custom atomicassetjs typings

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import UseAtomicAssetsProvider from './component/provider'
 
+// export all hooks
 export * from './hooks'
+// export atomicasset typings
+export * from './typings/atomicassets-js'
+// export provider
 export { UseAtomicAssetsProvider }
